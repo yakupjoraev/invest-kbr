bad4661caef63faa4acbf6dece9781e1544edac8
Yakup <yakupjoraev@gmail.com>
2024-03-16 19:56:16 +0300

    added invest-agency page

---
+++ build/js/index.bundle.js
@@ -0,0 +1,1 @@
+!function(){"use strict";!function(){const n=document.querySelector(".mobile-nav-btn"),o=document.querySelector(".nav"),c=document.querySelector(".nav-icon");n.onclick=function(){o.classList.toggle("nav--open"),c.classList.toggle("nav-icon--active"),document.body.classList.toggle("no-scroll")}}()}();
